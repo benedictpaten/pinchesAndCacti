d2ef5e5f58cd5a77847ce92eca6a2680e951dced
Joel Armstrong <jcarmstr@ucsc.edu>
2014-05-21 19:57:19 -0700

    Move bootstrap + neighbor-joining to sonLib

--- Makefile
+++ Makefile
@@ -5,23 +5,22 @@ libSources = impl/*.c
 libHeaders = inc/*.h
 libTests = tests/*.c
 testBin = tests/testBin
-quickTreeObjects = externalTools/quicktree_1.1/obj/buildtree.o externalTools/quicktree_1.1/obj/cluster.o externalTools/quicktree_1.1/obj/distancemat.o externalTools/quicktree_1.1/obj/options.o externalTools/quicktree_1.1/obj/sequence.o externalTools/quicktree_1.1/obj/tree.o externalTools/quicktree_1.1/obj/util.o
 
 all : externalToolsM ${libPath}/stPinchesAndCacti.a ${binPath}/stPinchesAndCactiTests
 
 externalToolsM : 
 	cd externalTools && make all
 
-${libPath}/stPinchesAndCacti.a : ${libSources} ${libHeaders} externalToolsM ${basicLibsDependencies}
-	${cxx} ${cflags} -I inc -I externalTools -I ${libPath}/ -c ${libSources}
-	ar rc stPinchesAndCacti.a *.o ${quickTreeObjects}
+${libPath}/stPinchesAndCacti.a : ${libSources} ${libHeaders} ${basicLibsDependencies}
+	${cxx} ${cflags} -I inc -I ${libPath}/ -c ${libSources}
+	ar rc stPinchesAndCacti.a *.o
 	ranlib stPinchesAndCacti.a 
 	rm *.o
 	mv stPinchesAndCacti.a ${libPath}/
 	cp ${libHeaders} ${libPath}/
 
 ${binPath}/stPinchesAndCactiTests : ${libTests} ${libSources} ${libHeaders} ${basicLibsDependencies} ${libPath}/3EdgeConnected.a
-	${cxx} ${cflags} -I inc -I impl -I externalTools -I${libPath} -o ${binPath}/stPinchesAndCactiTests ${libTests} ${libSources} ${basicLibs}  ${libPath}/3EdgeConnected.a ${quickTreeObjects}
+	${cxx} ${cflags} -I inc -I impl -I${libPath} -o ${binPath}/stPinchesAndCactiTests ${libTests} ${libSources} ${basicLibs}  ${libPath}/3EdgeConnected.a
 
 clean : 
 	cd externalTools && make clean
--- inc/stPinchPhylogeny.h
+++ inc/stPinchPhylogeny.h
@@ -72,54 +72,6 @@ stMatrix *stPinchPhylogeny_getMatrixFromBreakPoints(stList *featureColumns, stPi
  */
 stMatrix *stPinchPhylogeny_getSymmetricDistanceMatrix(stMatrix *matrix);
 
-// Data structure for storing information about a node in a
-// neighbor-joined tree.
-typedef struct {
-    int64_t matrixIndex;    // = -1 if an internal node, index into
-                            // distance matrix if a leaf.
-    int64_t *leavesBelow;   // leavesBelow[i] = 1 if leaf i is present
-                            // below this node, 0 otherwise. Could be a
-                            // bit array, which would make things much
-                            // faster.
-    int64_t numBootstraps;  // Number of bootstraps that support this split.
-    double bootstrapSupport;// Fraction of bootstraps that support this split.
-    int64_t totalNumLeaves; // Total number of leaves overall in the
-                            // tree (which is the size of
-                            // leavesBelow). Not strictly necessary,
-                            // but convenient
-} stPhylogenyInfo;
-
-// Create a tree using neighbor-joining, from a
-// (numSequences*numSequences) distance matrix. The leaves of the tree
-// are labeled by their index in the distance matrix. A
-// stPhylogenyInfo instance is stored in the clientData field of each
-// node, which must be freed.
-stTree *neighborJoin(double **distances, int64_t numSequences);
-
-// Free a stPhylogenyInfo struct.
-void stPhylogenyInfo_destruct(stPhylogenyInfo *info);
-
-// Free stPhylogenyInfo data on all nodes in the tree.
-void stPhylogenyInfo_destructOnTree(stTree *tree);
-
-// Return a new tree which has its partitions scored by how often they
-// appear in the bootstrap. This fills in the numBootstraps and
-// bootstrapSupport fields of each node. All trees must have valid
-// stPhylogenyInfo.
-stTree *scoreFromBootstrap(stTree *tree, stTree *bootstrap);
-
-// Return a new tree which has its partitions scored by how often they
-// appear in the bootstraps. This fills in the numBootstraps and
-// bootstrapSupport fields of each node. All trees must have valid
-// stPhylogenyInfo.
-stTree *scoreFromBootstraps(stTree *tree, stList *bootstraps);
-
-// Set (and allocate) the leavesBelow and totalNumLeaves attribute in
-// the phylogenyInfo for the given tree and all subtrees. The
-// phylogenyInfo structure (in the clientData field) must already be
-// allocated!
-void setLeavesBelow(stTree *tree, int64_t totalNumLeaves);
-
 // Returns a new tree with poorly-supported partitions removed. The
 // tree must have bootstrap-support information for every node.
 stTree *removePoorlySupportedPartitions(stTree *tree,
